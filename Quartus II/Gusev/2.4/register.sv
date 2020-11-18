088c0380cfc52876fcff3e17892faac0aba19461
IvanGusevv <71173705+IvanGusevv@users.noreply.github.com>
2020-11-18 20:30:06 +0700

    Update register.sv

--- Quartus II/Gusev/2.4/register.sv
+++ Quartus II/Gusev/2.4/register.sv
@@ -1,10 +1,10 @@
 	import Bits ::*;
 module register(DATA_OUT, A, B, C, CLK);
-	output reg [2*SIZE-1:0]DATA_OUT; /*1-цу не случайно забыл, когда складываем максимально возможные числа
+	output reg [2*SIZE:0]DATA_OUT; /*1-цу не случайно забыл, когда складываем максимально возможные числа
 	к получившееся числу добавится один старший разряд	*/
 	input reg [SIZE-1:0]A, B, C;
 	input wire CLK;
-	reg [2*SIZE-1:0]M;
+	reg [2*SIZE:0]M;
 	always @(posedge CLK)
 	begin
 	M <= A*B;
