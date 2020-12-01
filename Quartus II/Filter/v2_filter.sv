7ebe08a7a465510ba702393b45482e8065ed3bfc
IvanGusevv <71173705+IvanGusevv@users.noreply.github.com>
2020-12-01 16:25:25 +0700

    Update v2_filter.sv

--- Quartus II/Filter/v2_filter.sv
+++ Quartus II/Filter/v2_filter.sv
@@ -8,26 +8,25 @@ module v2_filter(	clk,
 	
 	input wire clk, reset;
 	input wire [SIZE_ADC_DATA+5:0]input_data;
-/* необходим 21 бит потому, что максимальное значение на выходе равно 293 527, на это нужно 20 бит
-и ещё 1 бит на знак + или -
+/* Neobhodimo 20 Bits, 19 chtobu vlezlo znachenie 293 527 i 1 Bit dl`a znaka
 */
 	output reg [SIZE_ADC_DATA + 5:0]output_data;
-/* в качестве переменной обозначены регистры с знакопеременными значениями
-потому, что в алгоритме приходится сохранять результаты предыдущих вычислений
+/* v kachestve peremennou oboznachenu registru c znakoperemennumu znacheniamu
+potomu, chto v algoritme prihodits`a sohranat` rezul`tatu predudyshih vuchesleniu
 */
-	reg signed [SIZE_ADC_DATA + 5:0] D[k+l:0];/* Этот массив регистров нужен для того, чтобы
-осуществить задержку входного сигнала на l, k, l+k шагов 
+	reg signed [SIZE_ADC_DATA + 5:0] D[k+l:0];/* etot massive registrov ny}I{en dl`a togo, chtobu
+osyshestvit` smeshenue vhodnogo cignala na l, k, l+k shagov 
 */
 	reg signed [SIZE_ADC_DATA + 5:0] p, d, s, r;
 	reg signed [SIZE_ADC_DATA + 5:0] D2, D1;
 	reg signed [SIZE_ADC_DATA + 5:0] M1;	
 	
-	always @(posedge clk or posedge reset)// пока тактовый сигнал действует и не подан сброс
+	always @(posedge clk or negedge reset)// poka clk  podan u sbros
 
 		begin
-	if (reset)
+			if (!reset)
 		begin
-// при активации сброса, данные обнуляются, а массив D необходимо отчистить
+// pri  sbrosa, dannue obnulauyt`sa, a massive D neobhodimo otchistit`
 		D[0] <= 0;
 		D1 <= 0;
 		D2 <= 0;
@@ -47,7 +46,7 @@ module v2_filter(	clk,
 
 	else
 	begin
-//если хочется задержку выходного сигнала в 1 такт, то =, если в l+1 такт, то <=
+
 		D[0] = input_data;
 		D1 = D[0] - D[l];
 		D2 = D[k] - D[k+l];
@@ -57,8 +56,8 @@ module v2_filter(	clk,
 		p = p + d;
 		r = p + M1;
 		s = s + r;
-		output_data <= s;// выводим выходной сигнал только после того как тактовый стгнал исчезнет
-		for (integer i = 1; i <= k+l; i++) // сдвигаем значение входного сигнала в следующий регистр
+		output_data <= s;
+		for (integer i = 1; i <= k+l; i++) // cdvigaem podanyu signal v sosedniu register
 		begin
 			D[i]<= D[i-1];
 		end
