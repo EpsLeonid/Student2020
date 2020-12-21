11abc1d88f69429f6669d5e004a091b03e54a354
ViktoriaEmets <70963917+ViktoriaEmets@users.noreply.github.com>
2020-12-21 10:54:39 +0700

    Add files via upload

--- Quartus II/Emets/task 2.4/registr.sv
+++ Quartus II/Emets/task 2.4/registr.sv
@@ -9,16 +9,16 @@ input reg [width-1:0]A,
 input reg [width-1:0]B,
 input reg [width-1:0]C,
 
-output reg [2*width-1:0]data,
-output reg [2*width-1:0]MULT,
-output reg [2*width-1:0]C1
+output reg [2*width-1:0]data
 );
-
+ reg [2*width-1:0]MULT;
+ reg [2*width-1:0]C1;
 parameter width=8;
 always @(posedge clk)
 
 begin
 MULT<=A*B;
+
 C1<=C;
 data <= MULT+C1;
 end
